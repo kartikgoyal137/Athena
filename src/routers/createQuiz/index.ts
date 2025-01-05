35ddf1334716182a2fd70f577a09accfc650c47f
Garv Makkar <garv.codes@gmail.com>
2025-01-05 21:02:01 +0530

    add searchManagers function

--- src/routers/createQuiz/index.ts
+++ src/routers/createQuiz/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import quizRouter from './quiz'
 import sectionRouter from './section'
 import questionRouter from './question'
+import managerRouter from './managers'
 
 const router = express.Router()
 
 router.use('/quiz', quizRouter)
 router.use('/section', sectionRouter)
 router.use('/question', questionRouter)
+router.use('/managers', managerRouter)
 
 export default router
