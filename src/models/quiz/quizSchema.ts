110400fa385287098cb3a2e2122fd083e3eed639
Adesh Gupta <adeshgupta101@gmail.com>
2025-01-23 01:17:16 +0530

    Added todo

--- src/models/quiz/quizSchema.ts
+++ src/models/quiz/quizSchema.ts
@@ -32,7 +32,7 @@ const quizSchema = new Schema<IQuiz>({
             },
             value: {
               type: String,
-              required: false,
+              required: false, //TODO: Do sanity checking for required fields 
             },
           },
         ],
