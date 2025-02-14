06dc9acc08c869d609fd81fee9be178c64023cb5
demigod <divyanshjain.2206@gmail.com>
2025-02-14 05:47:35 +0530

    Add clustering instances

--- src/server.ts
+++ src/server.ts
@@ -15,63 +15,78 @@ import mongoSanitize from 'express-mongo-sanitize'
 import logger from '@utils/logger'
 import profilePageRouter from '@routers/profilePage'
 import { FRONTEND_URL } from 'config'
+import cluster from 'cluster'
+import os from 'os'
 
-// Initialize server
-dotenv.config()
-connectDB()
+const numInstances = os.cpus().length
 
-const app: Express = express()
-const port = process.env.PORT
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
 
-// Middlewares
-app.use(cors({ origin: FRONTEND_URL, credentials: true }))
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(mongoSanitize())
-
-const server = http.createServer(app)
-
-// Middleware to access response body and log accordingly
-app.use((req: Request, res: Response, next: NextFunction) => {
-  // Override the res.send method to access the response
-  const originalSend = res.send
-  res.send = function (body) {
-    // Access the response here
-    if (res.statusCode === 401) {
-      logger.warn(
-        `Unauthorized Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
-      )
-    } else if (res.statusCode === 500) {
-      logger.error(
-        `Internal Server Error: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
-      )
-    } else if (res.statusCode === 400) {
-      logger.debug(`Bad Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
-    }
-    // Call the original send method to send the response to the user
-    return originalSend.call(this, body)
+  // Fork workers.
+  for (let i = 0; i < numInstances; i++) {
+    cluster.fork()
   }
-  next()
-})
 
-// Routers
-app.use('/auth', authRouter)
-app.use('/checkQuiz', checkQuizRouter)
-app.use('/createQuiz', createQuizRouter)
-app.use('/giveQuiz', giveQuizRouter)
-app.use('/common', commonRouter)
-app.use('/log', logRouter)
-app.use('/profile', profilePageRouter)
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`)
+  })
+} else {
+  // Initialize server
+  dotenv.config()
+  connectDB()
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
-})
+  const app: Express = express()
+  const port = process.env.PORT
 
-server.listen(port, () => {
-  logger.silly(`⚡️[server]: Server is running at http://localhost:${port}`)
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
-})
+  // Middlewares
+  app.use(cors({ origin: FRONTEND_URL, credentials: true }))
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(cookieParser())
+  app.use(mongoSanitize())
 
-export default app
+  const server = http.createServer(app)
+
+  // Middleware to access response body and log accordingly
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    // Override the res.send method to access the response
+    const originalSend = res.send
+    res.send = function (body) {
+      // Access the response here
+      if (res.statusCode === 401) {
+        logger.warn(
+          `Unauthorized Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
+        )
+      } else if (res.statusCode === 500) {
+        logger.error(
+          `Internal Server Error: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`,
+        )
+      } else if (res.statusCode === 400) {
+        logger.debug(`Bad Request: ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
+      }
+      // Call the original send method to send the response to the user
+      return originalSend.call(this, body)
+    }
+    next()
+  })
+
+  // Routers
+  app.use('/auth', authRouter)
+  app.use('/checkQuiz', checkQuizRouter)
+  app.use('/createQuiz', createQuizRouter)
+  app.use('/giveQuiz', giveQuizRouter)
+  app.use('/common', commonRouter)
+  app.use('/log', logRouter)
+  app.use('/profile', profilePageRouter)
+
+  app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server')
+  })
+
+  server.listen(port, () => {
+    logger.silly(`⚡️[server]: Server is running at http://localhost:${port}`)
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+  })
+}
